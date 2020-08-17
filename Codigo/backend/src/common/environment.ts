e7aeacd528fc8bf344764aed1b6283319bb5b7d2
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-08-16 23:04:37 -0300

    Add cors/dotenv/environments

---
+++ Codigo/backend/src/common/environment.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const environment = {
+  server: { port: process.env.SERVER_PORT || 3000 },
+  db: { url: process.env.DB_URL || 'mongodb://localhost/chat-api' },
+};
+
+export default environment;
