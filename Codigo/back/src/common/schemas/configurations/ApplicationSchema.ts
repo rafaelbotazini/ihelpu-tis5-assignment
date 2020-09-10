b05e4b7569591f307061da72dfc218c49303aa27
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-09-10 15:30:29 -0300

    deleted back end files

--- Codigo/back/src/common/schemas/configurations/ApplicationSchema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, SchemaOptions } from '@nestjs/mongoose';
-
-export function ApplicationSchema(options?: SchemaOptions): ClassDecorator {
-  return Schema({
-    id: true,
-    timestamps: true,
-    ...options,
-  });
-}
