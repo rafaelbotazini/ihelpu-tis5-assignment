88904449a367953afa323b2d2a97f0b72b0cd8c8
Rafael Botazini <rafaelbotazini@gmail.com>
2020-09-15 16:06:48 -0300

    Configure SPA routes

--- Codigo/front/src/App.tsx
+++ Codigo/front/src/App.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-const App: React.FC = () => (
-  <>
-    {/* <SignIn /> */}
-    <SignUp />
-    <GlobalStyle />
-  </>
-);
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import PrivateRoute from './pages/PrivateRoute';
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/app" />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <PrivateRoute path="/app">
+            <Switch>
+              <Route exact path="/app">
+                Home page
+              </Route>
+              <Route path="/app/*">404 Not found</Route>
+            </Switch>
+          </PrivateRoute>
+          <Route path="*">404 Not found</Route>
+        </Switch>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </React.Fragment>
+  );
+};
 
 export default App;
