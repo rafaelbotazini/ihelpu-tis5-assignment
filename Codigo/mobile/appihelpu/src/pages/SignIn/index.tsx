785d392aa84f47dccb5d91b06d98cafc18c0897e
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-10-02 17:10:02 -0300

    feat: added components

--- Codigo/mobile/appihelpu/src/pages/SignIn/index.tsx
+++ Codigo/mobile/appihelpu/src/pages/SignIn/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo@128px.png';
+
+import { Container, Title, SubTitle } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <SubTitle>iHelpU</SubTitle>
 
       <Title>Faça seu logon</Title>
 
+      <Input name="email" icon="mail" placeholder="E-mail"/>
+      <Input name="password" icon="lock" placeholder="Senha"/>
+
+      <Button onPress={() => { 
+          console.log('passed'); 
+        }}>
+          Entrar
+      </Button>
     </Container>
   );
 };
--- Codigo/mobile/appihelpu/src/pages/SignIn/styles.ts
+++ Codigo/mobile/appihelpu/src/pages/SignIn/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -11,4 +12,12 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 20px 0 24px;
+
 `;
