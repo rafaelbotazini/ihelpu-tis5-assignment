d33b29f124d9c80be6545c4ef9f13f588cb0b7d7
Rafael Botazini <rafaelbotazini@gmail.com>
2020-10-26 04:06:50 -0300

    Use enviroment rabbitmq connection URL

--- Codigo/back/src/modules/chat/chat.module.ts
+++ Codigo/back/src/modules/chat/chat.module.ts
@@ -2,17 +2,23 @@ import { Module } from '@nestjs/common';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ConfigModule } from 'modules/config/config.module';
+import { ConfigService } from 'modules/config/config.service';
 
 @Module({
   imports: [
-    RabbitMQModule.forRoot(RabbitMQModule, {
-      exchanges: [
-        {
-          name: 'chat_messages',
-          type: 'topic',
-        },
-      ],
-      uri: 'amqp://localhost:5672',
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        exchanges: [
+          {
+            name: 'chat_messages',
+            type: 'topic',
+          },
+        ],
+        uri: configService.get('RABBITMQ_URL'),
+      }),
     }),
   ],
   providers: [ChatService],
