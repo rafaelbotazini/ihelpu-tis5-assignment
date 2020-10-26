a9c5b4db06270922eaa03c4ed29f17971369d873
Rafael Botazini <rafaelbotazini@gmail.com>
2020-10-26 05:16:14 -0300

    Create logic for sending message from chat

--- Codigo/front/src/pages/Rooms/ChatPage/index.tsx
+++ Codigo/front/src/pages/Rooms/ChatPage/index.tsx
@@ -1,15 +1,29 @@
-import React, { useContext, useState } from 'react';
+import { backgrounds } from 'polished';
+import React, { useContext, useEffect, useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { useHistory, useParams } from 'react-router-dom';
 import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+import {
+  ConnectionStatusContext,
+  useConnectionStatus,
+} from '../../../contexts/ConnectionStatusContext';
+import { CurrentUserContext } from '../../../contexts/CurrentUserContext';
 import { UserGroupsContext } from '../../../contexts/UserGroupsContext';
+import { ChatMessagePayload } from '../../../models/ChatMessagePayload';
 import api from '../../../services/api';
+import mq from '../../../services/mq';
 import { Container } from './styles';
 
 const ChatPage: React.FC = () => {
   const history = useHistory();
   const { id } = useParams<{ id: string }>();
+  const { user } = useContext(CurrentUserContext);
   const { removeRoom } = useContext(UserGroupsContext);
+  const { connected } = useContext(ConnectionStatusContext);
+
+  const [messages, setMessages] = useState<ChatMessagePayload[]>([]);
+  const [textMessage, setTextMessage] = useState('');
 
   const handleGroupUnsubscribe = (): void => {
     api.rooms.leave(id).then(() => {
@@ -18,13 +32,52 @@ const ChatPage: React.FC = () => {
     });
   };
 
+  const handleKeypress = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter' && user && textMessage) {
+      mq.chatMessages.sendTextMessage(id, user.id, textMessage);
+      setTextMessage('');
+    }
+  };
+
+  useEffect(() => {
+    if (connected) {
+      const { unsubscribe } = mq.chatMessages.subscribeToChatMessages(
+        id,
+        (msg) => setMessages((msgs) => msgs.concat([JSON.parse(msg.body)])),
+      );
+      return unsubscribe;
+    }
+  }, [connected, id]);
+
   return (
     <Container>
-      <p>Chat ID: {id}</p>
-      <Button onClick={handleGroupUnsubscribe}>
-        <FaTimes />
-        Sair do grupo
-      </Button>
+      {!connected && 'Connectando...'}
+      {connected && (
+        <>
+          <p>Chat ID: {id}</p>
+
+          <div
+            style={{ marginBottom: 24, padding: 12, backgroundColor: '#000' }}
+          >
+            <pre>{JSON.stringify(messages, null, 2)}</pre>
+          </div>
+
+          <div>
+            <Input
+              name="message"
+              value={textMessage}
+              onChange={(e) => setTextMessage(e.target.value)}
+              onKeyPress={handleKeypress}
+              placeholder="Digite sua mensagem"
+            />
+          </div>
+
+          <Button onClick={handleGroupUnsubscribe}>
+            <FaTimes />
+            Sair do grupo
+          </Button>
+        </>
+      )}
     </Container>
   );
 };
