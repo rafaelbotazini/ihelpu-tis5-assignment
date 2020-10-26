66ba2fc1d3ec52e2427f7b50c4015dd83b6577d1
Rafael Botazini <rafaelbotazini@gmail.com>
2020-10-26 05:12:42 -0300

    Create message send and subscribe web client service

--- Codigo/front/src/services/wsRequest.ts
+++ Codigo/front/src/services/wsRequest.ts
@@ -3,21 +3,4 @@ import Stomp from 'stompjs';
 const ws = new WebSocket(`${process.env.REACT_APP_WEBSTOMP_URL}`);
 const client = Stomp.over(ws);
 
-client.connect(
-  'guest',
-  'guest',
-  () => {
-    console.log('client.connected', client.connected);
-    client.send(
-      '/exchange/chat_messages/send_text.42',
-      { 'content-type': 'text/plain' },
-      JSON.stringify({
-        userId: 'rafael@reactjs.client',
-        message: 'Olá :)',
-      }),
-    );
-  },
-  console.error,
-);
-
 export default client;
